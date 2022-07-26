23e786d2149731a2c8b50df96694ea1c061a1d60
crush598 <crush@tju.edu.cn>
2022-07-26 21:16:37 +0800

    优化

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,9 +1,8 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: 'one',link: '/one/',},
+  { text: '随笔',link: '/随笔/',},
   { text: 'two',link: '/two/',},
   { text: 'three', link: '/three/' },
   { text: '留言板', link: '/message-board/' },
-  { text: '我的博客', link: 'https://wiki.eryajf.net' },
 ]
