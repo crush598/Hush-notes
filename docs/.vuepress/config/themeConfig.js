1c69b7d9443bface3ae222c1c09eb32e4394e46d
crush598 <crush@tju.edu.cn>
2022-07-26 20:37:44 +0800

    优化

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -6,7 +6,7 @@ module.exports = {
   nav, // 顶部导航栏
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: 'https://cdn.staticaly.com/gh/eryajf/tu/main/img/image_20220720_132208.png', // 导航栏logo
-  repo: 'eryajf/Hush-notes', // 导航栏右侧生成Github链接
+  repo: 'crush598/Hush-notes', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   editLinks: true, // 启用编辑链接
@@ -40,8 +40,8 @@ module.exports = {
   },
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-    name: '二丫讲梵', // 必需
-    link: 'https://github.com/eryajf', // 可选的
+    name: 'Hush', // 必需
+    link: 'https://github.com/crush598', // 可选的
   },
   social: {
     // 社交图标，显示于博主信息栏和页脚栏
