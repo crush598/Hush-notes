144a8a632b036eb8d9d4f323ca36cfc484b34f99
crush598 <crush@tju.edu.cn>
2022-07-26 21:21:50 +0800

    优化

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,7 +1,7 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: '随笔',link: '/随笔/',},
+  { text: '随笔',link: '/01.随笔/',},
   { text: 'two',link: '/two/',},
   { text: 'three', link: '/three/' },
   { text: '留言板', link: '/message-board/' },
