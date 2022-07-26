f397ad4f07b695925a4b927f0db1da8a492af6fc
crush598 <crush@tju.edu.cn>
2022-07-26 20:45:23 +0800

    优化

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -6,7 +6,7 @@ module.exports = [
     'meta',
     {
       name: 'keywords',
-      content: '二丫讲梵,golang,vue,go-web,go-admin,go-ldap-admin',
+      content: 'Hush,golang,vue,go-web,go-admin,go-ldap-admin',
     },
   ],
   // ['meta', { name: 'baidu-site-verification', content: 'code-LTKHwOecxI' }], // 百度统计的站长验证
