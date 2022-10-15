9167d7504c883444ec0222d9aefd830a2857d3fb
Hush <crush@tju.edu.cn>
2022-10-15 21:40:42 +0800

    modify

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -4,12 +4,12 @@ const nav = require('./themeConfig/nav.js');
 // 主题配置
 module.exports = {
   nav, // 顶部导航栏
-  sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+  sidebarDepth: 1, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: 'https://cdn.jsdelivr.net/gh/crush598/image@main/二分/202208180906845.png', // 导航栏logo
   repo: 'crush598/Hush-notes', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
-  editLinks: true, // 启用编辑链接
+  editLinks: false, // 启用编辑链接
   docsDir: 'docs', // 编辑的文件夹
   docsBranch: 'main', // 编辑的分支
   editLinkText: '编辑',
@@ -18,7 +18,7 @@ module.exports = {
   //*** 以下配置是Vdoing主题改动和新增的配置 ***//
 
   category: false, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
-  tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
+  tag: true, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
   // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
   // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
 
